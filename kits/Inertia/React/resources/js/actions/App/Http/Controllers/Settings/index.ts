6932e9c640de3ef33d5ff1661fd61c2467934cc8
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-16 10:52:49 +0000

    Setup Inertia React and Vue variants

---
+++ kits/Inertia/React/resources/js/actions/App/Http/Controllers/Settings/index.ts
@@ -0,0 +1,11 @@
+import PasswordController from './PasswordController'
+import ProfileController from './ProfileController'
+import TwoFactorAuthenticationController from './TwoFactorAuthenticationController'
+
+const Settings = {
+    ProfileController: Object.assign(ProfileController, ProfileController),
+    PasswordController: Object.assign(PasswordController, PasswordController),
+    TwoFactorAuthenticationController: Object.assign(TwoFactorAuthenticationController, TwoFactorAuthenticationController),
+}
+
+export default Settings

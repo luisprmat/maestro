e617509143018a437bbce05bb7e453fa3c2edb79
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-01-22 16:25:54 +0000

    Add missing route name

--- kits/Inertia/WorkOS/Base/routes/web.php
+++ kits/Inertia/WorkOS/Base/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 
-Route::get('/', fn () => Inertia::render('{{welcome}}'));
+Route::get('/', fn () => Inertia::render('{{welcome}}'))->name('home');
 
 Route::middleware([
     'auth',

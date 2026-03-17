2a28fdb667666ce9b32dbc5a2654a729ecaa7952
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-03-17 14:58:28 +0000

    Laravel 13 (#44)
    * Update starter kits to support Laravel 13
    
    * Fix issue on welcome page SVG animations for Livewir3e
    
    * Set minimum-stability to stable across all kits

--- kits/Livewire/WorkOS/app/Models/User.php
+++ kits/Livewire/WorkOS/app/Models/User.php
@@ -4,37 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
+#[Fillable(['name', 'email', 'workos_id', 'avatar'])]
+#[Hidden(['workos_id', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'workos_id',
-        'avatar',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'workos_id',
-        'remember_token',
-    ];
 
     /**
      * Get the user's initials.
